b3dbe236e2677473ad3168f7abf8caee81cbeacf
Тимур Надршин <nadrshin.timur@mail.ru>
2025-05-16 02:00:10 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author:'Джордж Оруэлл',
+        name:'1984',
+        publicationYear: 1949,
+        price: 750,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author:'Маргарет Митчелл',
+        name:'Унесенные ветром',
+        publicationYear: 1936,
+        price: 550,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author:'Михаил Булгаков',
+        name:'Мастер и Маргарита',
+        publicationYear: 1967,
+        price: 800,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author:'Федор Достоевский',
+        name:'Преступление и наказание',
+        publicationYear: 1866,
+        price: 775,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author:'Фрэнсис Скотт Фицджеральд',
+        name:'Великий Гэтсби',
+        publicationYear: 1925,
+        price: 450,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author:'Дж. К. Роулинг',
+        name:'Гарри Поттер и филосовский камень',
+        publicationYear: 1967,
+        price: 850,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author:'Габриэль Гарсиа Маркес',
+        name:'Сто лет одиночества',
+        publicationYear: 1967,
+        price: 775,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author:'Эрих Мария Ремарк',
+        name:'Три товарища',
+        publicationYear: 1936,
+        price: 565,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author:'Умберто Эко',
+        name:'Имя розы',
+        publicationYear: 1980,
+        price: 500,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author:'Лев Толстой',
+        name:'Анна Каренина',
+        publicationYear: 1877,
+        price: 950,
+        bestseller: true,
+        inStock: false
+    },
+];
